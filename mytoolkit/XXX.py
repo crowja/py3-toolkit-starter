5e696682e9b0b9d9c77806229a089d541e61f27d
jac <crowja@gmail.com>
2021-12-28 14:53:32 -0600

    Tidy.

--- mytoolkit/XXX.py
+++ mytoolkit/XXX.py
@@ -2,12 +2,14 @@ import argparse
 
 
 def main():
-    ap = argparse.ArgumentParser(prog="mytoolkit XXX")
+    ap = argparse.ArgumentParser(prog="mytoolkit XXX")  # TODO
     ap.add_argument("name", help=argparse.SUPPRESS)
     ap.add_argument(
         "-o", "--outfile", default=False, help="Output file name, default stdout"
     )
     args = ap.parse_args()
+
+    # TODO now get up and do what needs to be done ...
 
 
 if __name__ == "__main__":
